ea0edd88c72af98c105fde4c50ffaf20320908a0
olivierV79 <olivier.villaverde@maif.fr>
2024-02-08 11:20:21 +0100

    exo12

--- chapter01/exercice01/index.js
+++ chapter01/exercice01/index.js
@@ -1,11 +1,19 @@
 
 export function greetWorld() {
-
+return 'Hey, world!';
 }
 
 export function greet(name) {
+    return "Hey, "+name+ "!";
 }
 
-export function capitilizedGreet(name) {
 
+export function capitilizedGreet(name) {
+    function strUcFirst(a) {
+        return a.charAt(0).toUpperCase() + a.substring(1).toLowerCase();
+        // return a.slice(0,1).toUpperCase() + a.slice(1).toLowerCase();
+      }
+    
+    name = strUcFirst(name);
+    return "Hey, "+name+ "!"
 }
--- chapter01/exercice02/index.js
+++ chapter01/exercice02/index.js
@@ -1,8 +1,22 @@
 export function sum(a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
+    return a+b;
 }
 
 export function isGreaterThan(input, comp) {
+    input = parseInt(input);
+    comp = parseInt(comp);
+    if(input>comp){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 export function reverseNumber(num) {
+    num = parseInt(num.toString().split("").reverse().join(""));
+
+    return num;
 }
