bf1a9149718ff7a4690a8e9d98c8cfadb8987055
olivierV79 <olivier.villaverde@maif.fr>
2024-02-08 15:00:36 +0100

    ffff

--- chapter01/exercice04/index.js
+++ chapter01/exercice04/index.js
@@ -1,10 +1,14 @@
 export function addWithCallback(a, b, callback) {
   setTimeout(() => {
+    let sum = a + b;
+    callback(sum);
   }, 1000);
 }
 
 export function filterArrayWithCallback(array, filterFunc, callback) {
   setTimeout(() => {
+    let res = array.filter(filterFunc);
+    callback(res);
   }, 1000);
 }
 
