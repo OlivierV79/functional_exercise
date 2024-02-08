50e9a004a166568c84aad93eb01ce9e7e4fad306
olivierV79 <olivier.villaverde@maif.fr>
2024-02-08 13:43:25 +0100

    exo3

--- chapter01/exercice03/index.js
+++ chapter01/exercice03/index.js
@@ -1,8 +1,31 @@
 export function reverseString(str) {
+    return str.split("").reverse().join("");
 }
 
 export function isPalindrome(str) {
+    str = str.replace(/\s/g,"");
+    let revStr = reverseString(str);
+    if(revStr===str){
+        return true
+    } else {
+        return false
+    }
+    
 }
 
 export function countVowels(str) {
+    
+    let count = 0;
+
+    for (let i=0; i < str.length; i++)
+    {
+        
+              
+        if (str.charAt(i) == "a" || str.charAt(i) == "e" || str.charAt(i) == "i" || str.charAt(i) == "o" || str.charAt(i) == "u" || str.charAt(i) == "é" || str.charAt(i) == "è" || str.charAt(i) == "y") {
+            count ++;
+        }
+
+    }
+
+    return count;
 }
